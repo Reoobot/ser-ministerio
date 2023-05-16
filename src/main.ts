45efef1bde4d0ca27f1855cbe06b0d76ab337652
Freddy Vegas <vegasfalexis@gmail.com>
2023-05-16 22:20:30 +0200

    24d3

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   
-  const app = await NestFactory.create(AppModule);
   
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api/v1")
   await app.listen(3000);
   app.enableCors()
 }
