b8aa436aa963ace56bbff5d78543726dd07c5600
Freddy Vegas <vegasfalexis@gmail.com>
2023-05-19 10:11:55 +0200

    dd

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   
   
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api/v1")
+  app.setGlobalPrefix("/api/v1")
   app.enableCors()
   await app.listen(3000);
 }
